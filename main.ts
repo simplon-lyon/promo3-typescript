30249328072101902e6a4328fa536c208dcbbe75
Jean <jdemel@simplon.co>
2017-09-12 17:26:31 +0200

    added CORS

--- main.ts
+++ main.ts
@@ -2,9 +2,12 @@ import { router } from './todo-router';
 import { userRouter } from './user-router';
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
 
 
 const app = express();
+
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
